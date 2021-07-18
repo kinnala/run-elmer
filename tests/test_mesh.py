de327d1a6a7ebc50a13a6d28bd5b7b90ea2ef684
Tom Gustafsson <tom.gustafsson@iki.fi>
2021-07-18 14:48:55 +0300

    Run a simple case in the test

--- tests/test_mesh.py
+++ tests/test_mesh.py
@@ -1,7 +1,51 @@
 import elmer
 
+poisson = """
+Check Keywords "Warn"
+
+Header
+  Mesh DB "." "."
+End
+
+Simulation
+  Max Output Level = 5
+  Coordinate System = Cartesian
+  Simulation Type = Steady
+  Output Intervals(1) = 1
+  Steady State Max Iterations = 1
+  Post File = "results.vtu"
+End
+
+Body 1
+  Equation = 1
+  Body Force = 1
+End
+
+Body Force 1 :: Source = Real 1.0
+Equation 1 :: Active Solvers(1) = 1
+
+Solver 1
+  Equation = "Poisson"
+  Variable = "Potential"
+  Variable DOFs = 1
+  Procedure = "Poisson" "PoissonSolver"
+  Linear System Solver = "Direct"
+  Linear System Direct Method = UMFPack
+  Steady State Convergence Tolerance = 1e-09
+End
+
+Boundary Condition 1
+  Target Boundaries(1) = 0
+  Potential = Real 0
+End
+
+"""
 
 def test_elmer():
-    m = elmer.MeshTri()
-    case = elmer.Case(m, "")
-    assert case.sif == ""
+    m = elmer.MeshTri().refined()
+    case = elmer.Case(m, poisson)
+    out = case.run(
+        fetch="results_t0001.vtu",
+        verbose=True,
+    )
+    assert len(results.points) == 25
